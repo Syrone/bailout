a6a2bc80bfa1a903cee43231320640de863968a6
Andrew Vasilchenko <cazonove@gmail.com>
2024-06-25 02:17:36 +0300

    added 5 blocks

--- src/js/components/swipers.js
+++ src/js/components/swipers.js
@@ -1,7 +1,34 @@
 import Swiper, { Pagination } from 'swiper';
 Swiper.use([Pagination]);
 
-const heroSwipers = document.querySelectorAll('.hero-swiper')
+const resizableSwiper = (breakpoint, swiperClass, swiperConfig, callback) => {
+	let swiper
+
+	breakpoint = window.matchMedia(breakpoint)
+
+	const enableSwiper = function(className, settings) {
+		swiper = new Swiper(className, settings)
+		
+		if (callback) callback(swiper)
+	}
+
+	const checker = function() {
+		if (breakpoint.matches) {
+			return enableSwiper(swiperClass, swiperConfig)
+		} else {
+			if (swiper !== undefined) swiper.destroy(true, true)
+			return
+		}
+	}
+
+	breakpoint.addEventListener('change', checker)
+	checker()
+}
+
+const heroSwipers = document.querySelectorAll('.hero-swiper'),
+			resizableSwipers = document.querySelectorAll('.resizable-swiper'),
+			resizableTripleSwipers = document.querySelectorAll('.resizable-swiper--triple'),
+			lawSwipers = document.querySelectorAll('.law-swiper')
 
 heroSwipers?.forEach((el) => {
 	const pagination = el.querySelector('.swiper-pagination')
@@ -9,7 +36,7 @@ heroSwipers?.forEach((el) => {
 	new Swiper(el, {
 		watchSlidesProgress: true,
 		pagination: {
-			el: '.swiper-pagination',
+			el: pagination,
 			clickable: true,
 		},
 
@@ -36,4 +63,74 @@ heroSwipers?.forEach((el) => {
 			}
 		}
 	});
+})
+
+resizableSwipers?.forEach((el) => {
+	const pagination = el.querySelector('.swiper-pagination')
+	resizableSwiper(
+		'(max-width: 75rem)',
+		el,
+		{
+			watchSlidesProgress: true,
+			slidesPerView: 'auto',
+			spaceBetween: 40,
+			pagination: {
+				el: pagination,
+				clickable: true,
+			},
+			breakpoints: {
+				0: {
+					slidesPerView: 'auto',
+					spaceBetween: 10
+				},
+				576: {
+					slidesPerView: 'auto',
+					spaceBetween: 10
+				},
+				768: {
+					slidesPerView: 'auto',
+					spaceBetween: 10
+				},
+				992: {
+					slidesPerView: 'auto',
+					spaceBetween: 24
+				},
+				1200: {
+					slidesPerView: 'auto',
+					spaceBetween: 32
+				},
+			}
+		}
+	)
+})
+
+lawSwipers?.forEach((el) => {
+	const pagination = el.querySelector('.swiper-pagination')
+	resizableSwiper(
+		'(max-width: 75rem)',
+		el,
+		{
+			watchSlidesProgress: true,
+			slidesPerView: 1,
+			spaceBetween: 40,
+			pagination: {
+				el: pagination,
+				clickable: true,
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 10,
+					slidesPerView: 1,
+				},
+				992: {
+					spaceBetween: 24,
+					slidesPerView: 1,
+				},
+				1200: {
+					spaceBetween: 32,
+					slidesPerView: 1,
+				},
+			}
+		}
+	)
 })
